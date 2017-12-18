8328ba63a4f7535f5a5e441a74f6aacf0dbd7c94
Francy Lang <francy.lang@gmail.com>
2017-12-18 15:06:49 -0700

    Display total count

--- public/scripts.js
+++ public/scripts.js
@@ -5,13 +5,24 @@ $(document).ready(() => {
 const appendItem = (item) => {
   $('.append-item').append(
     ` <div class="item">
-           <h2 class="item-title">${item.item}</h2>
-           <h2 class="item-reason">${item.reason}</h2>
-           <h2 class="item-cleanliness">${item.cleanliness}</h2>
-        </div>
+         <h2 class="item-title">${item.item}</h2>
+         <h2 class="item-reason">${item.reason}</h2>
+         <h2 class="item-reason">${item.cleanliness}</h2>
       </div>`
   );
-  console.log('here');
+}
+
+const displayCount = (items) => {
+  console.log(items);
+  $('.item-count').text(`Total Count: ${items.length}`);
+  $('.rancid-count').text(`Rancid: ${countCleanliness(items, 'rancid')}`);
+  $('.sparkling-count').text(`Sparkling: ${countCleanliness(items, 'sparkling')}`);
+  $('.dusty-count').text(`Dusty: ${countCleanliness(items, 'dusty')}`);
+}
+
+const countCleanliness = (items, status) => {
+  const filtered = items.filter(item => item.cleanliness === status);
+  return filtered.length;
 }
 
 const appendAllItems = (items) => {
@@ -23,6 +34,7 @@ const fetchItems = () => {
     .then(response => response.json())
     .then(storedItems => {
       appendAllItems(storedItems)
+      displayCount(storedItems)
     })
     .catch(error => console.log(error))
 }
@@ -40,10 +52,13 @@ const postItem = (event) => {
   })
     .then(response => response.json())
     .then(item => {
+      console.log(item);
       appendItem(item)
     })
     .catch(error => console.log(error))
 }
+
+
 
 
 $('.new-item-save').on('click', postItem);
